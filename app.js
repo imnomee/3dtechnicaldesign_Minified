6f980b08d2d9f81fc6e24593c482abb9ce8bcbbc
Noman Rafiq <heyimnomee@gmail.com>
2024-04-22 15:02:02 +0500

    minor changes

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ document.getElementById('date').innerHTML = new Date().getFullYear();
 
 const navBtn = document.getElementById('nav-toggle');
 const links = document.getElementById('nav-links');
+const link = document.querySelectorAll('.nav-link');
 
 navBtn.addEventListener('click', () => {
     console.log(links);
     console.log('clicked');
     links.classList.toggle('show-links');
+});
+
+link.forEach((l) => {
+    l.addEventListener('click', () => {
+        links.classList.remove('show-links');
+    });
 });
